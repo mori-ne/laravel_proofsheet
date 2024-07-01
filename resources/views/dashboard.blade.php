d1bf37ebff8979d932c40dc206af03c35eed7b43
morine <fold.it.fold.it@gmail.com>
2024-07-01 10:48:16 +0900

    CRUD delete

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -63,7 +63,7 @@
                                         action="{{ route('dashboard.destroy', $post->id) }}" method="post">
                                         @csrf
                                         @method('DELETE')
-                                        <button onclick="confirmDelete({{ $post->id }})" type="submit"
+                                        <button onclick="confirmDelete({{ $post->id }})" type="button"
                                             class="py-3 px-4 inline-flex items-center gap-x-2 -ms-px first:rounded-s-lg first:ms-0 last:rounded-e-lg text-sm font-medium focus:z-10 border border-gray-200 bg-white shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none text-red-500">
                                             削除
                                         </button>
